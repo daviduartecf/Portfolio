122bc5b57849889176a15b86ddf0113573923925
daviduartecf <152318399+daviduartecf@users.noreply.github.com>
2023-11-29 09:59:35 +0000

    Add files via upload

---
+++ ecommerce/ecommerce/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'gg.djangodemo@gmail.com'
+EMAIL_HOST_PASSWORD = 'gftqzvjodmhidaqm'
+EMAIL_PORT = 587
---
+++ ecommerce/static/js/site.js
@@ -0,0 +1,80 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get references to the login form and button
+    var loginFormContainer = document.getElementById("login-form-container");
+    var loginButton = document.getElementById("login-button");
+
+    // Show the login form when the button is clicked
+    loginButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        loginFormContainer.style.display = "block";
+    });
+
+    // You can add additional logic to handle form submission (e.g., AJAX request)
+    //var loginForm = document.getElementById("login-form");
+    //loginForm.addEventListener("submit", function (event) {
+       // event.preventDefault();
+        // Add your login logic here, for example, using AJAX to submit the form data
+        //console.log("Form submitted. Handle login logic here.");
+    //});
+
+
+    const productsContainer = document.querySelector('.products-container');
+    const scrollLeftButton = document.querySelector('.scroll-button.left');
+    const scrollRightButton = document.querySelector('.scroll-button.right');
+    const scrollAmount = 10; // Adjust this value for the desired scroll speed
+    const maxWidth = 700;
+    
+    scrollLeftButton.style.display = 'none'; // Initially hide the left-scroll button
+    
+    function animateScroll(scrollTo) {
+        const currentScroll = productsContainer.scrollLeft;
+        const difference = scrollTo - currentScroll;
+        const step = Math.sign(difference) * scrollAmount;
+        
+    
+        console.log(`Current Scroll: ${currentScroll}`);
+        console.log(`Scroll To: ${scrollTo}`);
+        console.log(`Difference: ${difference}`);
+        console.log(`Step: ${step}`);
+    
+        if (Math.abs(difference) <= Math.abs(step)) {
+            productsContainer.scrollLeft = scrollTo;
+        } else {
+            productsContainer.scrollLeft += step;
+    
+            // Adjust the scroll direction based on scrollTo and currentScroll
+            if (scrollTo < currentScroll && step > 0 ) {
+                productsContainer.scrollLeft = scrollTo;
+            } else if (scrollTo > currentScroll && step < 0) {
+                productsContainer.scrollLeft = scrollTo;
+            } else {
+                requestAnimationFrame(() => animateScroll(scrollTo));
+            }
+        }
+    }
+    
+    
+    
+    productsContainer.addEventListener('scroll', () => {
+        if (productsContainer.scrollLeft > 0) {
+            scrollLeftButton.style.display = 'flex'; // Show the left-scroll button
+        } else {
+            scrollLeftButton.style.display = 'none'; // Hide the left-scroll button
+        }
+    });
+    
+    scrollLeftButton.addEventListener('click', () => {
+        const scrollTo = productsContainer.scrollLeft - 235; // Adjust the scroll distance as needed
+        animateScroll(scrollTo);
+    });
+    
+    scrollRightButton.addEventListener('click', () => {
+        if (productsContainer.scrollLeft < maxWidth) {
+        const scrollTo = productsContainer.scrollLeft + 235; // Adjust the scroll distance as needed
+        animateScroll(scrollTo);
+        }
+    });
+    
+
+});
